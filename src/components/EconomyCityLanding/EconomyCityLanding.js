4107873a938d025d22268493ca1df168d838f662
nguyenquangtrung8 <nguyenquangtrung8@gmail.com>
2025-03-14 20:57:38 +0700

    Update EconomyCityLanding.js

--- src/components/EconomyCityLanding/EconomyCityLanding.js
+++ src/components/EconomyCityLanding/EconomyCityLanding.js
@@ -1,67 +1,60 @@
 // src/components/EconomyCityLanding/EconomyCityLanding.js
 
-import React, { Suspense } from 'react';
+import React from 'react';
 import styles from './EconomyCityLanding.module.css';
-import Hero from '../Hero/Hero'; // Component Hero không cần lazy loading
-import Features from '../Features/Features'; // Import trực tiếp Features
-import Products from '../Products/Products'; // Import trực tiếp Products
-
-// Lazy load các component khác
-const TypicalFloor = React.lazy(() => import('../TypicalFloor/TypicalFloor'));
-const Gallery = React.lazy(() => import('../Gallery/Gallery'));
-const Contact = React.lazy(() => import('../Contact/Contact'));
-const Legal = React.lazy(() => import('../Legal/Legal'));
-const SalesPolicy = React.lazy(() => import('../SalesPolicy/SalesPolicy'));
-const PriceList = React.lazy(() => import('../PriceList/PriceList'));
-const FAQ = React.lazy(() => import('../FAQ/FAQ'));
+import Hero from '../Hero/Hero';
+import Features from '../Features/Features';
+import Products from '../Products/Products';
+import TypicalFloor from '../TypicalFloor/TypicalFloor';
+import Gallery from '../Gallery/Gallery';
+import Contact from '../Contact/Contact';
+import Legal from '../Legal/Legal';
+import SalesPolicy from '../SalesPolicy/SalesPolicy';
+import PriceList from '../PriceList/PriceList';
+import FAQ from '../FAQ/FAQ';
 
 const EconomyCityLanding = () => {
   return (
     <div className={styles.landingWrapper}>
-      {/* Component Hero không cần lazy loading */}
       <div id="hero">
         <Hero />
       </div>
       
-      {/* Features và Products không cần Suspense vì đã import trực tiếp */}
       <div id="features">
         <Features />
       </div>
-      
+
       <div id="products">
         <Products />
       </div>
-      
-      {/* Sử dụng Suspense để bọc các component lazy còn lại */}
-      <Suspense fallback={<div>Loading...</div>}>
-        <div id="typicalFloor">
-          <TypicalFloor />
-        </div>
 
-        <div id="legal">
-          <Legal />
-        </div>
+      <div id="typicalFloor">
+        <TypicalFloor />
+      </div>
 
-        <div id="priceList">
-          <PriceList />
-        </div>
+      <div id="legal">
+        <Legal />
+      </div>
 
-        <div id="salesPolicy">
-          <SalesPolicy />
-        </div>
+      <div id="priceList">
+        <PriceList />
+      </div>
 
-        <div id="gallery">
-          <Gallery />
-        </div>
+      <div id="salesPolicy">
+        <SalesPolicy />
+      </div>
 
-        <div id="FAQ">
-          <FAQ />
-        </div>
+      <div id="gallery">
+        <Gallery />
+      </div>
 
-        <div id="contact">
-          <Contact />
-        </div>
-      </Suspense>
+      <div id="FAQ">
+        <FAQ />
+      </div>
+
+      <div id="contact">
+        <Contact />
+      </div>
     </div>
   );
 };
